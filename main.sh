54c5fc06e89fe8c7a794422daa55e0166d2f2a68
andrea <andrea@localhost.localdomain>
2020-09-21 15:31:58 +0200

    ora e' ok

--- main.sh
+++ main.sh
@@ -3,18 +3,21 @@
 [ $# -lt 3 ] && echo "Usage: $0 <BPM> <left> <right>" && exit 1
 
 dt=$(echo "60/$1" | bc -l)
-duty_cycle=0.5
- left=$(echo "$dt/$2*$duty_cycle" | bc -l)
-right=$(echo "$dt/$3*$duty_cycle" | bc -l)
+pulse=0.5
+wait=$(echo 1-$pulse | bc -l)
+left_pulse=$(echo "$dt/$2*$pulse" | bc -l)
+left_wait=$(echo "$dt/$2*$wait" | bc -l)
+right_pulse=$(echo "$dt/$3*$pulse" | bc -l)
+right_wait=$(echo "$dt/$3*$wait" | bc -l)
 
 while true; do
 	for i in $(seq $2); do
-		echo left $i
-		play -nc2 synth $left  sine 440 remix 1 0 &>/dev/null
+		play -nc2 synth $left_pulse sine 440 remix 1 0 &>/dev/null
+		sleep $left_wait
 	done &
 	for i in $(seq $3); do
-		echo right $i
-		play -nc2 synth $right sine 440 remix 0 1 &>/dev/null
+		play -nc2 synth $right_pulse sine 440 remix 0 1 &>/dev/null
+		sleep $right_wait
 	done &
-	sleep $dt
+	wait
 done
